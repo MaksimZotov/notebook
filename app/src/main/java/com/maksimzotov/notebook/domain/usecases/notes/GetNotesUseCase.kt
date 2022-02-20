7457ef39161369d2b2e9040e25e136aca1685e16
MaksimZotov <zotovmaksim1254@gmail.com>
2022-02-20 15:59:52 +0300

    sorting

--- app/src/main/java/com/maksimzotov/notebook/domain/usecases/notes/GetNotesUseCase.kt
+++ app/src/main/java/com/maksimzotov/notebook/domain/usecases/notes/GetNotesUseCase.kt
@@ -1,12 +1,24 @@
 package com.maksimzotov.notebook.domain.usecases.notes
 
+import com.maksimzotov.notebook.di.qualifiers.DispatcherDefault
 import com.maksimzotov.notebook.domain.Repository
 import com.maksimzotov.notebook.domain.entities.note.Note
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
+import java.util.*
 import javax.inject.Inject
 
 class GetNotesUseCase @Inject constructor(
     private val repository: Repository,
+    @DispatcherDefault private val coroutineDispatcher: CoroutineDispatcher
 ) {
-    fun getNotes(): Flow<List<Note>> = repository.notes
+    fun getNotes(): Flow<List<Note>> = repository.notes.map { notes ->
+        withContext(coroutineDispatcher) {
+            notes.sortedByDescending { note ->
+                note.time
+            }
+        }
+    }
 }
